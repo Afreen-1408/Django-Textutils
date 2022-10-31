f775f8d474c26c8297aa46ec122b112f3ab6149d
Y Name <maniharafreen1408@gmail.com>
2022-10-31 09:57:17 +0530

    Django Textutils

---
+++ textutlis/textutlis/views.py
@@ -0,0 +1,65 @@
+# i have create my websie - Afreen 
+from random import randrange
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def analyze(request):
+    # Get the text:
+    djtext= request.POST.get('text', 'default')
+
+
+    removepunc= request.POST.get('removepunc', 'off')
+    fullcaps= request.POST.get('fullcaps', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    
+    
+
+    if removepunc == "on":
+        # analyzed=djtext
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': "Removing Punctuations", 'analyzed_text': analyzed}
+        djtext=analyzed
+
+
+
+    if(fullcaps=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': "Changr to uppercase", 'analyzed_text': analyzed}
+        djtext=analyzed
+       
+    
+    if(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+        params = {'purpose': "Extra space remover", 'analyzed_text': analyzed}
+        djtext=analyzed
+        
+
+    if (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char!="\r":
+                analyzed = analyzed + char
+            else:
+                print("no")
+        print("pre", analyzed)
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on"):
+        return HttpResponse("please select any operation and try again")
+
+    return render(request, 'analyze.html', params)
